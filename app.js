1d1235600908f50adfaff275c816a67f6449ab8b
Flying-Spaghetti-Monster69 <felipegarzon908@gmail.com>
2022-07-18 11:34:33 -0500

    express init

--- app.js
+++ app.js
@@ -1,1 +1,34 @@
-console.log('04 Store API')
+require('dotenv').config();
+
+// async error handling
+
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000
+
+const notFoundMiddleware = require('./middleware/not-found')
+const errorMiddleware = require('./middleware/error-handler')
+
+// middleware
+
+app.use(express.json());
+
+// routes
+app.get('/',(req,res)=>{
+    res.send('<h1>Welcome to the Store API</h1><a href="/api/v1/products">products route</a>')
+})
+
+// products route
+
+app.use(notFoundMiddleware);
+app.use(errorMiddleware);
+
+const start = async() =>{
+    try {
+        app.listen(port,console.log(`listening on port ${port}!`))
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+start()
