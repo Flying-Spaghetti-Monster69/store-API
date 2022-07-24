6b857b9aa7a787edf838cb0fadf913fe2a3a95e5
Flying-Spaghetti-Monster69 <felipegarzon908@gmail.com>
2022-07-24 16:22:49 -0500

    select added

--- controllers/products.js
+++ controllers/products.js
@@ -1,14 +1,12 @@
 const Product = require('../models/product')
 
 const getAllProductsStatic = async (req,res) =>{
-    const products = await Product.find({
-        featured: true,
-    })
+    const products = await Product.find({}).select('name price')
     res.status(200).json({products, nbHits:products.length})
 }
 
 const getAllProducts = async ( req,res) =>{
-    const {featured, company, name, sort} = req.query
+    const {featured, company, name, sort,select} = req.query
     const queryObject = {}
 
     if (featured){
@@ -21,11 +19,16 @@ const getAllProducts = async ( req,res) =>{
         queryObject.name = {$regex: name, $options: 'i'}
     }
     let result = Product.find(queryObject)
+    // sort
     if (sort) {
         const sortList =sort.split(',').join(' ')
         result = result.sort(sortList)
     }else{
         result = result.sort("createdAt")
+    }
+    if (select) {
+        const fieldsList = select.split(',').join(' ')
+        result = result.select(fieldsList)
     }
     const products =  await result
     res.status(200).json({products, nbHits:products.length})
