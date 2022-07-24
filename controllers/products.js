d659080e6684ef116a6e824b97cebb06f5be6cf3
Flying-Spaghetti-Monster69 <felipegarzon908@gmail.com>
2022-07-24 16:44:26 -0500

    limit, page and skip added

--- controllers/products.js
+++ controllers/products.js
@@ -30,6 +30,13 @@ const getAllProducts = async ( req,res) =>{
         const fieldsList = select.split(',').join(' ')
         result = result.select(fieldsList)
     }
+
+    const page = Number(req.query.page) || 1
+    const limit = Number(req.query.limit) || 5
+    const skip = (page-1) *limit;
+    
+    result = result.skip(skip).limit(limit)
+
     const products =  await result
     res.status(200).json({products, nbHits:products.length})
 }
